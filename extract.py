7d7dd1162c13be77ccb52870fe59328faeb92084
Yahya Badran <techtweaking@gmail.com>
2022-09-25 17:26:05 +0200

    Fix missing some words with not_wanted POS

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import pandas as pd
 
 bad_tags = ["archaic",  "obsolete" ]
+bad_tags = []
 bad_pos = set(['name', 'symbol', 'prefix', 'suffix', 'character'])
 
 verb_tense = ['past', 'present']
@@ -31,13 +32,14 @@ class Word():
 class AllWords:
     """Class for keeping track of an item in inventory."""
     words = {}
-
+    all_words = []
 
 
 def oneline(line, tmp):
     d = json.loads(line)
 
     word = d['word']
+    tmp.all_words.append(word.lower())
     if len(word.split()) > 1:
         return None
     pos = d['pos']
@@ -71,12 +73,15 @@ def read(file_path, freq_path):
     with open(file_path, 'r') as f:
         for line in f:
             w = oneline(line, data)
+            
             if w:
-                data.words[w.word] = data.words.get(w.word, [])
-                data.words[w.word].append(w.pos)
+                data.words[w.word.lower()] = data.words.get(w.word.lower(), [])
+                data.words[w.word.lower()].append(w.pos)
                 ALL_POSES.add(w.pos.lower())
                 #if len(data.words) > 10000:
                 #    break 
+
+    data.all_words = set(data.all_words)
 
     df = pd.read_csv(freq_path)
     #TODO insure feqs are low letter
@@ -87,12 +92,15 @@ def read(file_path, freq_path):
     data.words = {k: data.words[k] for k in sorted(data.words.keys(), reverse=True, key=lambda k: freqs[k.lower()])}
 
     with open('out.csv', 'w') as f, open('human.txt', 'w') as hf:
+        good_words = []
         for word, poses in data.words.items():
             if set(poses) - bad_pos:
                 pass
             else:
                 continue
 
+            if word.lower() == 'rape':
+                print(word)
             if not head:
                 x = ','.join(ALL_POSES)
                 x = 'count,' + x
@@ -104,8 +112,17 @@ def read(file_path, freq_path):
             x = ','.join(['1' if x in poses else '0' for x in ALL_POSES])
             x = str(freqs[word.lower()]) + ',' + x
             x = word + ',' + x
+            good_words.append(word.lower())
+
             f.write(x + '\n')
             hf.write(word + ': ' + str(poses) + '\n')
+
+
+        skipped_words = [w for w in data.all_words if freqs.get(w.lower(),0) > 1000 and w.lower() not in good_words]
+        skipped_words = sorted(skipped_words, reverse=True, key=lambda x: freqs.get(x.lower(),0))
+        skipped_words = [w + ':' + str(freqs[w.lower()]) for w in skipped_words]
+        f = open('bad_words.txt', 'w')
+        f.write('\n'.join(skipped_words))
 
 if __name__ == '__main__':
     read(sys.argv[1], sys.argv[2])
